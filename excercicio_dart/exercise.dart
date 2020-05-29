9364d8842a38f6e4791719eb68914f0a9b6512cd
lino.veloso <lino.veloso@dextra-sw.com>
2020-05-29 12:58:16 -0300

    Map and for

--- excercicio_dart/exercise.dart
+++ excercicio_dart/exercise.dart
@@ -17,4 +17,23 @@ main() {
   var nomes = ['Lino', 'Veloso', 'Junior'];
   print(nomes.length);
   print(nomes);
+
+
+  Set<int> conjunto = {0, 1, 2 ,3, 4,4,4,4};
+  print(conjunto.length);
+  print(conjunto);
+  print(conjunto is Set);
+
+  Map<String, double> notasDosAlunos = {
+    "Lino": 9.7
+  };
+Map<String, double> outraNota = {"Cláudio": 4.8};
+  print(notasDosAlunos);
+
+  notasDosAlunos.addAll(outraNota);
+print(notasDosAlunos);
+for (var chave in notasDosAlunos.values) {
+  print(chave);
+}
+
 }
