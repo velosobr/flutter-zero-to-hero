893f42631d24144697cdd923111a09908e796fc3
lino.veloso <lino.veloso@dextra-sw.com>
2020-08-04 12:55:19 -0300

    Adicionando Transação

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -4,6 +4,10 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -25,8 +29,9 @@ class TransactionForm extends StatelessWidget {
               children: <Widget>[
                 FlatButton(
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                   final value = double.tryParse(valueController.text) ?? 0.0;
+                   onSubmit(title, value);
                   },
                   child: Text(
                     'Nova Transação',
