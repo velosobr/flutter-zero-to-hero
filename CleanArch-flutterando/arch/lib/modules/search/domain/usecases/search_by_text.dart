f3ecfda80dd1c15744862b15459d056dd880d9c6
velosobr <linoc.veloso@gmail.com>
2020-08-09 11:02:35 -0300

    wip - Domain

--- CleanArch-flutterando/arch/lib/modules/search/domain/usecases/search_by_text.dart
+++ CleanArch-flutterando/arch/lib/modules/search/domain/usecases/search_by_text.dart
@@ -1,16 +1,20 @@
 import 'package:arch/modules/search/domain/entity/result_search.dart';
 import 'package:arch/modules/search/domain/errors/errors.dart';
+import 'package:arch/modules/search/domain/repositories/search_repository.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class SearchByText {
-  Future <Either<FailureSearch, List<ResultSearch>>> call(String searchText);
+  Future<Either<FailureSearch, List<ResultSearch>>> call(String searchText);
 }
 
 class SearchByTextImpl implements SearchByText {
+  final SearchRepository repository;
+
+  SearchByTextImpl(this.repository);
+
   @override
-  Future<Either<FailureSearch, List<ResultSearch>>> call(String searchText) async {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<FailureSearch, List<ResultSearch>>> call(
+      String searchText) async {
+    return repository.search(searchText);
   }
-  
 }
