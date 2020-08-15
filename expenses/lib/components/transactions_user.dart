4feaac506fb45d41de6a77041954e9da9079bfa2
velosobr <linoc.veloso@gmail.com>
2020-08-14 22:51:34 -0300

    just adjusting some files in the project

--- expenses/lib/components/transactions_user.dart
+++ expenses/lib/components/transactions_user.dart
@@ -41,8 +41,8 @@ class _TransactionUserState extends State<TransactionUser> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        TransactionList(_transactions),
         TransactionForm(_addTransaction),
+        TransactionList(_transactions),
       ],
     );
   }
