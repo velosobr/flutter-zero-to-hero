04d97cc687332b8f9264fb6826ac35cec3d7ad4f
velosobr <linoc.veloso@gmail.com>
2021-07-01 22:05:16 -0300

    splashscreen update with new brand

--- ragnbills/lib/shared/themes/app_colors.dart
+++ ragnbills/lib/shared/themes/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static final primary = Color(0xFFFF941A);
+  static final primary = Color(0xFF0493CD);
   static final grey = Color(0xFF585666);
   static final delete = Color(0xFFE83F5B);
   static final heading = Color(0xFF585666);
