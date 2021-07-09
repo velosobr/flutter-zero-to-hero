187a7101fd3748dedb268ee818cf786e96ab1dee
velosobr <linoc.veloso@gmail.com>
2021-07-09 20:34:32 -0300

    WIP LoginPage

--- ragnbills/lib/modules/login/login_page.dart
+++ ragnbills/lib/modules/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ragnbills/shared/themes/app_colors.dart';
 import 'package:ragnbills/shared/themes/app_images.dart';
+import 'package:ragnbills/shared/themes/app_text_styles.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -17,24 +18,44 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: AppColors.background,
       body: Container(
         width: size.width,
-        height: size.width,
+        height: size.height,
         child: Stack(
           children: [
             Container(
               width: size.width,
-              height: size.height * 0.36,
+              height: size.height * 0.30,
               color: AppColors.primary,
             ),
             Positioned(
-              top: 40,
+                top: 40,
+                left: 0,
+                right: 0,
+                child: Image.asset(
+                  AppImages.person,
+                  width: 208,
+                  height: 373,
+                )),
+            Positioned(
+              bottom: size.height * 0.25,
               left: 0,
               right: 0,
-              child: Image.asset(
-                AppImages.person,
-                width: 208,
-                height: 373,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
+                    ),
+                  )
+                ],
               ),
-            )
+            ),
           ],
         ),
       ),
